ba330d561e6b658b3c7913eaae91040612961852
NISHANT SINGH <shubhamnishant04@gmail.com>
2025-08-13 15:35:30 +0530

    added the files

---
+++ JAVA FUNDAMENTALS/addTwoNumbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class addTwoNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+        System.out.println("Sum = " + (num1 + num2));
+    }
+}
