6b5d3c439f2201d8f3e4680073777d943c8eadee
NISHANT SINGH <shubhamnishant04@gmail.com>
2025-08-21 16:48:14 +0530

    working with number format exception

---
+++ String_practice/NumberFormatDemo.java
@@ -0,0 +1,31 @@
+package String_practice;
+
+import java.util.Scanner;
+
+public class NumberFormatDemo {
+
+    public static void generateException(String text) {
+        int number = Integer.parseInt(text); // might throw NumberFormatException
+        System.out.println("Number: " + number);
+    }
+
+    public static void handleException(String text) {
+        try {
+            int number = Integer.parseInt(text);
+            System.out.println("Number: " + number);
+        } catch (NumberFormatException e) {
+            System.out.println("Caught NumberFormatException: " + e.getMessage());
+        } catch (RuntimeException e) {
+            System.out.println("Caught RuntimeException: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String text = sc.next();
+
+        //generateException(text);
+        handleException(text);
+    }
+}
