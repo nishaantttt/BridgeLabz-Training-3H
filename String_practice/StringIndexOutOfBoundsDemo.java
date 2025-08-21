099093c895d4fd6cf8138de6df5343b924e8ab99
NISHANT SINGH <shubhamnishant04@gmail.com>
2025-08-21 16:49:17 +0530

    index out of bound process explaination

---
+++ String_practice/StringIndexOutOfBoundsDemo.java
@@ -0,0 +1,27 @@
+package String_practice;
+
+import java.util.Scanner;
+
+public class StringIndexOutOfBoundsDemo {
+
+    public static void generateException(String text) {
+        System.out.println("Char: " + text.charAt(text.length() + 1));
+    }
+
+    public static void handleException(String text) {
+        try {
+            System.out.println("Char: " + text.charAt(text.length() + 1));
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("Caught StringIndexOutOfBoundsException: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String text = sc.next();
+
+        //generateException(text);  // Uncomment to see the crash
+        handleException(text);
+    }
+}
