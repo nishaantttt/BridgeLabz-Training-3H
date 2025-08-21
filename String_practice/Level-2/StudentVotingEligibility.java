b408ade25b91d94400b37c09f0854f7591384dff
NISHANT SINGH <shubhamnishant04@gmail.com>
2025-08-22 00:57:10 +0530

    [Nishant] create- random age generator and voting eligibility checker

---
+++ String_practice/Level-2/StudentVotingEligibility.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class StudentVotingEligibility {
+    public static int[] generateRandomAges(int n) {
+        Random rand = new Random();
+        int[] ages = new int[n];
+        for (int i = 0; i < n; i++) {
+            ages[i] = rand.nextInt(90) + 10; // random age 10 to 99 (2-digit)
+        }
+        return ages;
+    }
+    public static String[][] checkVotingEligibility(int[] ages) {
+        String[][] result = new String[ages.length][2];
+        for (int i = 0; i < ages.length; i++) {
+            result[i][0] = String.valueOf(ages[i]);
+            if (ages[i] < 0) {
+                result[i][1] = "false";
+            } else {
+                result[i][1] = (ages[i] >= 18) ? "true" : "false";
+            }
+        }
+        return result;
+    }
+    public static void displayTable(String[][] arr) {
+        System.out.println("Age\tCan Vote");
+        for (String[] row : arr) {
+            System.out.println(row[0] + "\t" + row);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+        int[] ages = generateRandomAges(n);
+        String[][] eligibility = checkVotingEligibility(ages);
+        displayTable(eligibility);
+        sc.close();
+    }
+}
