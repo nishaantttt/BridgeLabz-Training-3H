ff2459fbc3d145795136e52866c78939ba62455a
NISHANT SINGH <shubhamnishant04@gmail.com>
2025-08-21 16:59:36 +0530

    Implements manual uppercase conversion using ASCII values

---
+++ UppercaseComparison.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class UppercaseComparison {
+
+    // Convert manually using ASCII values
+    public static String toUpperManual(String text) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                sb.append((char)(c - 32));
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    // Compare strings character by character
+    public static boolean compareStrings(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) return false;
+        }
+        return true;
+    }
+
+}
