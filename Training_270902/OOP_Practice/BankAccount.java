9bea109ad0739a620977921cb5593da35043cfaf
NISHANT SINGH <shubhamnishant04@gmail.com>
2025-09-04 16:33:01 +0530

    [Nishant] Add:atm bank account problem statement solution

---
+++ Training_270902/OOP_Practice/BankAccount.java
@@ -0,0 +1,40 @@
+package Training_270902.OOP_Practice;
+
+public class BankAccount {
+    String accountHolder;
+    int accountNumber;
+    double balance;
+
+    public BankAccount(String accountHolder, int accountNumber, double balance) {
+        this.accountHolder = accountHolder;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println(amount + " deposited successfully.");
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println(amount + " withdrawn successfully.");
+        } else if (amount > balance) {
+            System.out.println("Insufficient balance.");
+        } else {
+            System.out.println("Invalid withdrawal amount.");
+        }
+    }
+
+    public void displayBalance() {
+        System.out.println("Account Holder: " + accountHolder);
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Current Balance: " + balance);
+        System.out.println("---------------------------");
+    }
+}
