4d77b5c62c999ebb674632bc827873845ce2a6b2
NISHANT SINGH <shubhamnishant04@gmail.com>
2025-09-04 16:23:23 +0530

    [Nishant] Add: items caluclation solution

---
+++ Training_270902/OOP_Practice/Item.java
@@ -0,0 +1,21 @@
+public class Item {
+    int itemCode;
+    String itemName;
+    double price;
+
+    public Item(int itemCode, String itemName, double price) {
+        this.itemCode = itemCode;
+        this.itemName = itemName;
+        this.price = price;
+    }
+
+    public void displayItem() {
+        System.out.println("Item Code: " + itemCode);
+        System.out.println("Item Name: " + itemName);
+        System.out.println("Price: " + price);
+    }
+
+    public double calculateTotalCost(int quantity) {
+        return price * quantity;
+    }
+}
---
+++ Training_270902/OOP_Practice/oopsMain.java
@@ -0,0 +1,14 @@
+package Training_270902.OOP_Practice;
+
+// import Training_270902.OOP_Practice.oops.newclass;
+
+public class oopsMain {
+    public static void main(String[] args) {
+        oops oopsconst = new oops();
+        oops.newclass eg = new oops.newclass();
+        System.out.println(eg.number);
+        // newclass newer = new newclass();
+        // System.out.println(newer.number);
+    }
+
+}
