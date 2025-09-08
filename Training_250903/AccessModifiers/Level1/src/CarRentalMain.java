042af25586781e99589b5f546c700dab8db2170f
NISHANT SINGH <shubhamnishant04@gmail.com>
2025-09-08 05:34:50 +0530

    [Nishant] Add:car rental system

--- Training_250903/AccessModifiers/Level1/src/CarRentalMain.java
+++ Training_250903/AccessModifiers/Level1/src/CarRentalMain.java
@@ -1,7 +1,7 @@
 public class CarRentalMain {
     public static void main(String[] args) {
         CarRental rental1 = new CarRental();
-        System.out.println("Default Rental: " + rental1);
+        System.out.println("Default Rental: " + rental1); 
 
         CarRental rental2 = new CarRental("Alice", "Toyota Camry", 5, 2000.0);
         System.out.println("Parameterized Rental: " + rental2);
